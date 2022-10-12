53e38dfb06ad35f237d2e4783cac9e1b77dba75e
v <v>
2022-10-12 09:06:47 +0200

    vav commit version fonctionnelle sur plusieurs variables

--- client/client-with-encryption.py
+++ client/client-with-encryption.py
@@ -16,7 +16,33 @@ cert_idx = 1
 cert=f"vincent/my_cert.der"
 private_key=f"vincent/my_private_key.pem"
 
-
+async def browse_nodes(node: Node):
+    """
+    Build a nested node tree dict by recursion (filtered by OPC UA objects and variables).
+    """
+    node_class = await node.read_node_class()
+    children = []
+    for child in await node.get_children():
+        if await child.read_node_class() in [ua.NodeClass.Object, ua.NodeClass.Variable]:
+            children.append(
+                await browse_nodes(child)
+            )
+    if node_class != ua.NodeClass.Variable:
+        var_type = None
+    else:
+        try:
+            var_type = (await node.read_data_type_as_variant_type()).value
+        except ua.UaError:
+            _logger.warning('Node Variable Type could not be determined for %r', node)
+            var_type = None
+    return {
+        'id': node.nodeid.to_string(),
+        'name': (await node.read_display_name()).Text,
+        'cls': node_class.value,
+        'children': children,
+        'type': var_type,
+    }
+	
 async def task(loop):
 	url = "opc.tcp://127.0.0.1:4840/freeopcua/server/"
 	#url = "opc.tcp://admin@127.0.0.1:4840/freeopcua/server/"
@@ -35,11 +61,76 @@ async def task(loop):
 	
 	async with client:
 		objects = client.nodes.objects
-		child = await objects.get_child(['0:MyObject', '0:MyVariable'])
+		uri = "http://esigelec.ddns.net"
+		idx = await client.get_namespace_index(uri)
+		print ("idx="+str(idx))
+		#child = await objects.get_child([str(idx)+':MyObject', str(idx)+':MyVariable'])
+		child = await objects.get_child([str(idx)+':MyObject', str(idx)+':MyVariable'])
+		childA = await objects.get_child([str(idx)+':MyObject', str(idx)+':MyVariableA'])
+		
 		print(await child.get_value())
+		print(await childA.get_value())
 		await child.set_value(42.0)
+		await childA.set_value(-42.0)
 		print(await child.get_value())
-		print(await child.get_value())
+		print(await childA.get_value())
+		
+		print ("try child 2")
+		print ("dir(objects)="+str(dir(objects)))
+		print ("objects.nodeid="+str(objects.nodeid))
+		
+		
+		root = client.nodes.root
+		_logger.info("Objects node is: %r", root)
+		
+		
+
+		# Node objects have methods to read and write node attributes as well as browse or populate address space
+		_logger.info("Children of root are: %r", await root.get_children())
+		
+		
+		#print ("get_variables ="+str(root.get_variables()))
+		#print ("get_children_description ="+str(root.get_children_description()))
+		
+		
+		children = []
+		for child in await root.get_children():
+			print ("child="+str(child))
+		
+		#print (children)
+			
+		"""node=client.nodes.objects
+		node_class = await node.read_node_class()
+		children = []
+		print ("get child")
+		children = []
+		chidlId=0
+		for child in await node.get_children():
+			if await child.read_node_class() in [ua.NodeClass.Object, ua.NodeClass.Variable]:
+				if chidlId <10:
+					children.append(
+						await browse_nodes(child)
+					)
+				childId+=1
+				
+		print ("childreens="+str(children))
+		
+		#tree = await browse_nodes(client.nodes.objects)
+		#_logger.info('Node tree: %r', tree)
+		await client.disconnect()
+		#browse_recursive(root)
+		"""
+		
+		
+		
+		objects2 = client.nodes.objects
+		child2 = await objects2.get_child([str(idx)+':MyObject2', str(idx)+':MyVariable2'])
+		#child2 = await objects2.get_child([str(87)+':MyObject2', str(87)+':MyVariable2'])
+		print(await child2.get_value())
+		await child2.set_value(43.0)
+		print(await child2.get_value())
+		print(await child2.get_value())
+		
 		print ("finish")
 
 
--- server/server-with-encryption.py
+++ server/server-with-encryption.py
@@ -13,6 +13,45 @@ logging.basicConfig(level=logging.INFO)
 
 ENCRYPT=True
 
+
+
+class MyRobot():
+	def __init__(self,server,robotName):
+	
+		self.server =server
+		
+		"""async with self.server:
+			await self.server.nodes.objects.add_object(idx, robotName)
+			self.pose = await myobj.add_variable(0, "pose", 0.0)
+			self.poseVal=0.0
+			await self.pose.set_writable()
+		"""
+		pass
+		
+	def doGesture(self):
+	
+		#ecriture de la nouvelle pose
+		self.poseVal+=1.0
+		"""async with self.server:
+			await self.pose.write_value(self.poseVal)
+		"""	
+		pass
+		
+class RobotsGesture():
+	def __init__(self,server):
+		self.server =server
+		
+		#add entry points
+		self.robotList=[]
+		self.robotList.append(MyRobot(server,"Segway"))
+		
+		pass
+		
+	def doGesture(self):
+		for robot in self.robotList:
+			robot.doGesture()
+		pass
+
 async def main():
 
 	cert_user_manager = CertificateUserManager()
@@ -21,7 +60,9 @@ async def main():
 	#await cert_user_manager.add_user("vincent/my_cert.der", name='VAUCHEY')
 	#look her to know how to configure it
 	await cert_user_manager.add_admin("vincent/my_cert.der", name='VAUCHEY')
+	
 
+	
 	if ENCRYPT:
 		server = Server(user_manager=cert_user_manager)
 	else:
@@ -45,21 +86,31 @@ async def main():
 		await server.load_certificate("vincent/my_cert.der")
 		await server.load_private_key("vincent/my_private_key.pem")
 		
-	idx = 0
-
+	#idx = 0
+	uri = "http://esigelec.ddns.net"
+	idx = await server.register_namespace(uri)
+	print ("idx="+str(idx))
 	# populating our address space
 	myobj = await server.nodes.objects.add_object(idx, "MyObject")
 	myvar = await myobj.add_variable(idx, "MyVariable", 0.0)
+	myvarA = await myobj.add_variable(idx, "MyVariableA", 0.0)
 	await myvar.set_writable()  # Set MyVariable to be writable by clients
+	
+	myobj2 = await server.nodes.objects.add_object(idx, "MyObject2")
+	myvar2 = await myobj2.add_variable(idx, "MyVariable2", 2.0)
+	await myvar2.set_writable()  # Set MyVariable to be writable by clients
 
 	# starting!
+	#robotsGesture =RobotsGesture(server)
 
 	async with server:
 		while True:
 			await asyncio.sleep(1)
 			current_val = await myvar.get_value()
+			#await robotsGesture.doGesture()#call the robot gesture 
 			count = current_val + 0.1
 			await myvar.write_value(count)
+			await myvarA.write_value(-count)
 
 
 if __name__ == "__main__":
